9d044f49ae5085d6c11cad4999e321170ad47763
Luiza Amaral Trindade <98432851+luizatrindade@users.noreply.github.com>
2023-10-15 18:27:18 -0300

    Update Análise Fatorial e TRI.r

--- Análise Fatorial e TRI.r
+++ Análise Fatorial e TRI.r
@@ -8,7 +8,8 @@ library(mirt)
 
 
 #carregamento de dados-link no readme
-
+data <- read_delim("localdoarquivo")
+  
 #analise H inicial
 dataH <-data[,1:40]
 Theta<-matrix(seq(-5,5, by=.1))
@@ -26,4 +27,23 @@ irtH <- mirt(dataH,1, itemtype = "gpcm", SE = TRUE)
 M2(irtH)
 itemfit(irtH)
 coef(irtH,simplify = TRUE, IRTpars = TRUE)$items
+
+#retirar tudo com "a" (discriminacao) abaixo de 0.34 em modulo, itens com baixissima discriminacao 
+
+dataH2<-dataH[,c(2:7,11,17:20,24:27,30:31, 35:37,39,40)]
+
+myModelH2 <- '
+   # latent variables
+    H2 =~  HSinc2 + HSinc3 + HSinc4 + HSinc5 + HSinc6 + HSinc7 + HFair1 +  HFair7 + HFair8 + HFair9 + HFair10+ HGree4 + HGree5 + HGree6 + HGree7 + HGree10+HMode1 + HMode5 + HMode6 + HMode7 + HMode9 + HMode10
+   
+'
+
+fit2 <- cfa(model = myModelH2,
+           data  = dataH2, estimator="WLS")
+summary (fit2, fit.measures = TRUE)
+
+irtH2 <- mirt(dataH2,1, itemtype = "gpcm", SE = TRUE)
+M2(irtH2)
+itemfit(irtH2)
+coef(irtH2,simplify = TRUE, IRTpars = TRUE)$items
 
